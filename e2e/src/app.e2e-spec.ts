c892b2e82aecb57438e77ca61de5659ec8be10ed
FacundoSar <facundosargiotti@gmail.com>
2018-09-11 20:36:12 -0300

    New Angular/cli

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to fundacion-bajo-boulogne!');
+    expect(page.getParagraphText()).toEqual('Welcome to FundacionBajoBoulogne!');
   });
 });
